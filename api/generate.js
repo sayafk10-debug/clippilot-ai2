1183f695880a739bdfb46567356eac29312bdd05
sayafk10-debug <sayafk10@gmail.com>
2026-07-04 21:25:04 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -132,7 +132,7 @@ export default async function handler(req, res) {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "meta-llama/llama-3.3-70b-instruct:free",
+          model: "deepseek/deepseek-chat-v3-0324:free",
           messages: [
             {
               role: "system",
